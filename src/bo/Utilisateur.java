86ce83c181ce288fc8416e6e4a5995fd4d5fbe18
Flavie Rabillard <flavrabi@gmail.com>
2021-01-21 15:17:11 +0100

    update BO Utilisateur

--- src/bo/Utilisateur.java
+++ src/bo/Utilisateur.java
@@ -255,6 +255,32 @@ public class Utilisateur {
 		this.administrateur = administrateur;
 		this.listeArticles = listeArticles;
 	}
+	/**
+	 * @param pseudo
+	 * @param nom
+	 * @param prenom
+	 * @param email
+	 * @param telephone
+	 * @param rue
+	 * @param codePostal
+	 * @param ville
+	 * @param motDePasse
+	 */
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue,
+			String codePostal, String ville, String motDePasse) {
+		super();
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+	}
+	
+	
 	
 	
 
