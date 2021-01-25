76b73147322ba467c65ace6e0fcf8da4cda01a05
woyoy <woyoy@DESKTOP-S8GOKK6.home>
2021-01-25 18:54:36 +0100

    mise à jour articleDAO

--- src/dal/ArticleVenduDAO.java
+++ src/dal/ArticleVenduDAO.java
@@ -19,19 +19,9 @@ public class ArticleVenduDAO {
 	private static final String INSERT = " INSERT INTO ARTICLES_VENDUS (nom_article, description, date_debut_enchere,"
 			+ " date_fin_enchere, prix_initial, no_categorie, no_utilisateur)" +
 			" VALUES (?,?,?,?,?,?,?)";
-	private static final String SELECT =" SELECT " + 
-			
-			"	no_article," + 
-			"	nom_article," +
-			"	prix_initial," +
-			"	date_fin_enchere," + 
-			"	no_utilisateur," + 
-			"	pseudo" + 
-			"	FROM ARTICLES_VENDUS "; 
-			//"	INNER JOIN  ENCHERES e ON e.a.no_article=a.no_article" +
-			//"	INNER JOIN UTILISATEURS u ON u.no_utilisateur=a.no_utilisateur" +
-			//"	ORDER BY date_fin_enchere desc";	
-
+	
+	private  final  static  String  SELECT =  "select * from ARTICLES_VENDUS a"
+			+ 	"INNER JOIN UTILISATEURS u ON u.no_utilisateur=a.no_utilisateur where etat_vente = 'EC'"; 
 
 	public ArticleVendu insert(ArticleVendu article) throws SQLException {
 		
@@ -73,30 +63,59 @@ public class ArticleVenduDAO {
 	
 
 
-	public List<ArticleVendu> select() throws Exception {
-		List<ArticleVendu> listeArticlesVendus = new ArrayList<ArticleVendu>();
-		ArticleVendu articleVendu = new ArticleVendu();
+	public static  List <ArticleVendu> select () throws BuisnessException {
 
-		try(Connection cnx = ConnectionProvider.getConnection())
-		{
-			Statement stmt = cnx.createStatement();
-			ResultSet rs = stmt.executeQuery(SELECT);
-			while(rs.next())
-			{
-				articleVendu.setNomArticle(rs.getString("nom_article"));
-				articleVendu.setMiseAPrix(rs.getInt("prix_initial"));
+		ArticleVendu articleVendu = new ArticleVendu ();
+		List <ArticleVendu> listeRetourArticle =  new ArrayList <> ();
+		Connection cnx = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			cnx = ConnectionProvider.getConnection();
+			stmt = cnx . createStatement ();
+			rs = stmt . executeQuery (SELECT);
+
+			while (rs . next ()) {
+				articleVendu = new ArticleVendu();
+				articleVendu.setNoArticle(rs.getInt ("no_article"));
+				articleVendu.setNomArticle(rs.getString ("nom_article"));
+				articleVendu.setDescription(rs.getString ("description"));
+				articleVendu.setDateDebutEnchere(LocalDateTime.of((rs.getDate("date_debut_enchere").toLocalDate()), rs.getTime("date_debut_enchere").toLocalTime()));
 				articleVendu.setDateFinEnchere(LocalDateTime.of((rs.getDate("date_fin_enchere").toLocalDate()), rs.getTime("date_fin_enchere").toLocalTime()));
-				//enchereLu.setPseudo(rs.getString("pseudo"));
+				articleVendu.setMiseAPrix(rs.getInt("prix_initial"));
+				articleVendu.setPrixVente (rs.getInt("prix_vente"));
+				articleVendu.setEtatVente (rs.getString("etat_vente"));
+				articleVendu.setNoCategorie(rs.getInt("no_categorie"));
 
-				listeArticlesVendus.add(articleVendu);	
+				Utilisateur vendeur = new Utilisateur();
+				//vendeur.setNoUtilisateur(rs.getInt("no_utilisateur"));
+				vendeur.setPseudo(rs.getString("pseudo"));
+				//vendeur.setNom(rs.getString("nom"));
+				//vendeur.setPrenom(rs.getString("prenom"));
+				//vendeur.setEmail(rs.getString("email"));
+				//vendeur.setTelephone(rs.getString("telephone"));
+				//vendeur.setRue(rs.getString("rue"));
+				//vendeur.setCodePostal(rs.getString("code_postal"));
+				//vendeur.setVille(rs.getString("ville"));
+				//vendeur.setMotDePasse(rs.getString("mot_de_passe"));
+				//vendeur.setCredit(rs.getInt("credit"));
+				//vendeur.setAdministrateur(rs.getBoolean("administrateur"));
+				articleVendu.setUtilisateur(vendeur);
+
+				listeRetourArticle.add(articleVendu);
+
 			}
-		}
-		catch(Exception e)
-		{
+		} catch ( SQLException e) {		
 			e.printStackTrace();
-
-			throw e;
 		}
-		return listeArticlesVendus;
-	}
+		try {
+			if (stmt != null ) stmt.close();
+			if (cnx != null ) cnx.close();
+		} catch ( SQLException e) {
+			e.printStackTrace();
+		}
+
+		return listeRetourArticle;		
+	}		
+	
 }
